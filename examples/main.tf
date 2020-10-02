11df88a1054186a256d85e1f0057c40c730d908b
jack1902 <39212456+jack1902@users.noreply.github.com>
2020-10-02 17:09:00 +0100

    Adding an examples folder
    - Creates pipeline, team and agent token

---
+++ examples/main.tf
@@ -0,0 +1,28 @@
+provider "buildkite" {}
+
+resource "buildkite_team" "test" {
+    name = "terraform_provider_test"
+
+    privacy = "VISIBLE"
+    default_team = false
+    default_member_role = "MEMBER"
+}
+
+resource "buildkite_pipeline" "repo2" {
+    name = "terraform_provider_buildkite_pipeline"
+    repository = "git@github.com:org/repo2"
+    steps = file("./steps.yml")
+
+    team {
+        slug = buildkite_team.test.slug
+        access_level = "READ_ONLY"
+    }
+}
+
+resource "buildkite_agent_token" "fleet" {
+    description = "token used by build fleet"
+}
+
+output "agent_token" {
+    value = buildkite_agent_token.fleet.token
+}
