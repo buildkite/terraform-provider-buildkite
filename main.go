2c7aa3504e9b4375d0dfcde735ef03844f47319f
James Healy <james@yob.id.au>
2020-12-17 15:10:05 +1100

    go fmt

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/buildkite/terraform-provider-buildkite/buildkite"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/buildkite/terraform-provider-buildkite/buildkite"
 )
 
 func main() {
