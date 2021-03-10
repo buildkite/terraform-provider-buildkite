af055e668791875a4c55f608c9aeb58da948bc7f
James Healy <james@buildkite.com>
2021-03-10 23:11:17 +1100

    remove hard coded org slug from the PipelineSchedule acceptance tests
    We accidentally left a hard coded slug in here, which means this test
    fails when run against any other organization. That wasn't our intention
    - all contributors should be able to run the tests against their own
    test organization.
    
    Fixes #121

--- buildkite/resource_pipeline_schedule_test.go
+++ buildkite/resource_pipeline_schedule_test.go
@@ -3,6 +3,7 @@ package buildkite
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -257,7 +258,7 @@ func testAccCheckPipelineScheduleRemoteValues(resourcePipeline *PipelineNode, re
 
 func testAccGetImportPipelineScheduleSlug(resourceSchedule *PipelineScheduleNode) resource.ImportStateIdFunc {
 	return func(s *terraform.State) (string, error) {
-		slug := fmt.Sprintf("%s/%s/%s", "buildkite-terraform-provider-test-org", "test-pipeline-foo", resourceSchedule.UUID)
+		slug := fmt.Sprintf("%s/%s/%s", os.Getenv("BUILDKITE_ORGANIZATION"), "test-pipeline-foo", resourceSchedule.UUID)
 		return slug, nil
 	}
 }
