da3ab41bc4ed661e6a92e6832adc381c0e71569c
jradtilbrook <jarryd@tilbrooktech.com>
2022-06-10 10:29:13 +0800

    SUP-191 Add test analytics uploader (commented out)

--- .buildkite/steps/annotate.sh
+++ .buildkite/steps/annotate.sh
@@ -5,3 +5,13 @@ go test -v -cover -json ./... | tee test_output
 tparse -all -file test_output | tee tparse_output
 
 printf '```term\n%b\n```' "$(cat tparse_output)" | buildkite-agent annotate --style info
+
+# buildkite-test-analytics-go < test_output \
+#   --ci buildkite \
+#   --key "$BUILDKITE_BUILD_ID" \
+#   --build-number "$BUILDKITE_BUILD_NUMBER" \
+#   --job-id "$BUILDKITE_JOB_ID" \
+#   --branch "$BUILDKITE_BRANCH" \
+#   --commit-sha "$BUILDKITE_COMMIT" \
+#   --message "$BUILDKITE_MESSAGE" \
+#   --build-url "$BUILDKITE_BUILD_URL"
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM golang:1.18.2@sha256:04fab5aaf4fc18c40379924674491d988af3d9e97487472e674d0b
 
 RUN apt-get update \
     && apt-get install -y unzip \
-    && go install github.com/mfridman/tparse@latest
+    && go install github.com/mfridman/tparse@latest \
+    && go install github.com/lox/buildkite-test-analytics-go@latest
 
 ENV TERRAFORM_VERSION=0.14.11
 
