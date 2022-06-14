3deb6775122a23248cb14562a82e26eca151d00e
jradtilbrook <jarryd@tilbrooktech.com>
2022-06-14 10:19:34 +0800

    SUP-191 Upload to test analytics

--- .buildkite/steps/annotate.sh
+++ .buildkite/steps/annotate.sh
@@ -6,12 +6,12 @@ tparse -all -file test_output | tee tparse_output
 
 printf '```term\n%b\n```' "$(cat tparse_output)" | buildkite-agent annotate --style info
 
-# buildkite-test-analytics-go < test_output \
-#   --ci buildkite \
-#   --key "$BUILDKITE_BUILD_ID" \
-#   --build-number "$BUILDKITE_BUILD_NUMBER" \
-#   --job-id "$BUILDKITE_JOB_ID" \
-#   --branch "$BUILDKITE_BRANCH" \
-#   --commit-sha "$BUILDKITE_COMMIT" \
-#   --message "$BUILDKITE_MESSAGE" \
-#   --build-url "$BUILDKITE_BUILD_URL"
+buildkite-test-analytics-go < test_output \
+  --ci buildkite \
+  --key "$BUILDKITE_BUILD_ID" \
+  --build-number "$BUILDKITE_BUILD_NUMBER" \
+  --job-id "$BUILDKITE_JOB_ID" \
+  --branch "$BUILDKITE_BRANCH" \
+  --commit-sha "$BUILDKITE_COMMIT" \
+  --message "$BUILDKITE_MESSAGE" \
+  --build-url "$BUILDKITE_BUILD_URL"
