20f8e32c9f1cf4ec9cbe833eee42b4e1e352e6c8
Jarryd Tilbrook <jarryd@buildkite.com>
2022-07-13 11:15:01 +0800

    SUP-191 Generate test coverage (#206)
    * SUP-191 Update docker-compose plugin and mount agent
    
    * SUP-191 Move ci steps to a script
    
    * SUP-191 Add more env vars to docker-compose
    
    * SUP-191 Add test analytics uploader (commented out)
    
    * SUP-191 Add analytics token to acceptance tests
    
    * SUP-191 Upload to test analytics

---
+++ .buildkite/steps/annotate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+go test -v -cover -json ./... | tee test_output
+
+tparse -all -file test_output | tee tparse_output
+
+printf '```term\n%b\n```' "$(cat tparse_output)" | buildkite-agent annotate --style info
+
+buildkite-test-analytics-go < test_output \
+  --ci buildkite \
+  --key "$BUILDKITE_BUILD_ID" \
+  --build-number "$BUILDKITE_BUILD_NUMBER" \
+  --job-id "$BUILDKITE_JOB_ID" \
+  --branch "$BUILDKITE_BRANCH" \
+  --commit-sha "$BUILDKITE_COMMIT" \
+  --message "$BUILDKITE_MESSAGE" \
+  --build-url "$BUILDKITE_BUILD_URL"
--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM golang:1.18.3@sha256:a452d6273ad03a47c2f29b898d6bb57630e77baf839651ef77d03e4e049c5bf3
 
 RUN apt-get update \
-    && apt-get install -y unzip
+    && apt-get install -y unzip \
+    && go install github.com/mfridman/tparse@latest \
+    && go install github.com/lox/buildkite-test-analytics-go@latest
 
 ENV TERRAFORM_VERSION=0.14.11
 
