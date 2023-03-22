7246c2e3a105fc1840072d5f5b03955ea5fb83e7
Ben McNicholl <benmcnicholl@gmail.com>
2023-03-23 09:44:32 +1100

    Add terraform base

--- tf-proj/main.tf
+++ tf-proj/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     buildkite = {
       source  = "buildkite/buildkite"
-      version = "0.11.1"
+      version = "0.12.0"
     }
   }
 }
@@ -15,6 +15,11 @@ resource "buildkite_pipeline" "test" {
   repository = "https://github.com/buildkite/terraform-provider-buildkite.git"
 
   steps = ""
+}
+
+resource "buildkite_organization_settings" "test_settings" {
+  slug = buildkite_pipeline.test.organization_slug
+  allowed_api_ip_addresses = "0.0.0.0/0"
 }
 
 resource "buildkite_pipeline_schedule" "foo" {
