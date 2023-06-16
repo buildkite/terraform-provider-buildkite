f855aa62dd717133c0fd6a8e844f1be080dc1275
Jarryd Tilbrook <jarryd@buildkite.com>
2023-06-16 15:15:47 +0800

    Spike converting to framework (#284)
    * Add github.com/hashicorp/terraform-plugin-mux
    
    * Add other dependencies
    
    * Add github.com/hashicorp/terraform-plugin-framework
    
    * Add mux server

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/buildkite/terraform-provider-buildkite/buildkite"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
 
 // Set at compile time from ldflags
@@ -12,9 +18,47 @@ var (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return buildkite.Provider(version)
+	ctx := context.Background()
+
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	upgradedSdkServer, err := tf5to6server.UpgradeServer(
+		ctx,
+		buildkite.Provider(version).GRPCProvider,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkServer
 		},
-	})
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+
+	if debug {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/buildkite/buildkite",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
