2878e7c8850f3acd91d4448f099e026b85607d96
Ben McNicholl <benmcnicholl@gmail.com>
2023-06-20 16:19:39 +1000

    Initial Cluster resource work

---
+++ buildkite/resource_cluster.go
@@ -0,0 +1,104 @@
+package buildkite
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type ClusterResource struct {
+	client *Client
+}
+
+type ClusterResourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+	Emoji       types.String `tfsdk:"emoji"`
+	Color       types.String `tfsdk:"color"`
+}
+
+type ClusterResourceAPIModel struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Emoji       string `json:"emoji"`
+	Color       string `json:"color"`
+}
+
+func (c *ClusterResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = "_cluster"
+}
+
+func (c *ClusterResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "A Cluster is a group of Agents that can be used to run your builds. " +
+			"Clusters are useful for grouping Agents by their capabilities, such as operating system, hardware, or location. ",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "The name of the Cluster. Can only contain numbers and letters, no spaces or special characters.",
+				Required:            true,
+			},
+			"description": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "A description for the Cluster. Consider something short but clear on the Cluster's function.",
+			},
+			"emoji": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "An emoji to represent the Cluster. Accepts the format :buildkite:.",
+			},
+			"color": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "A color representation of the Cluster. Accepts hex codes, eg #BADA55.",
+			},
+		},
+	}
+}
+
+func (c *ClusterResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data *ClusterResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	createReq := ClusterCreateInput{
+		Name:        data.Name.ValueString(),
+		Description: data.Description.ValueString(),
+		Emoji:       data.Emoji.ValueString(),
+		Color:       data.Color.ValueString(),
+	}
+
+	r, err := createCluster(c.client.genqlient, createReq)
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create Cluster",
+			fmt.Sprintf("Unable to create Cluster: %s", err.Error()),
+		)
+		return
+	}
+
+	data.ID = types.StringValue(r.ClusterCreate.Cluster.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (c *ClusterResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	/* ... */
+}
+
+func (c *ClusterResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	/* ... */
+}
+
+func (c *ClusterResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	/* ... */
+}
