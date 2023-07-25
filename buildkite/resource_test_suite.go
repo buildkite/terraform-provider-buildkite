18b01b0f064e1f30fdafac43784233cee3e7ac33
Jarryd Tilbrook <jarryd@buildkite.com>
2023-07-25 20:17:53 +0800

    WIP test_suite with blocks

--- buildkite/resource_test_suite.go
+++ buildkite/resource_test_suite.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
@@ -18,9 +19,9 @@ type testSuiteModel struct {
 	ApiToken      types.String `tfsdk:"api_token"`
 	DefaultBranch types.String `tfsdk:"default_branch"`
 	ID            types.String `tfsdk:"id"`
+	UUID          types.String `tfsdk:"uuid"`
 	Name          types.String `tfsdk:"name"`
 	Slug          types.String `tfsdk:"slug"`
-	WebUrl        types.String `tfsdk:"web_url"`
 	Team          []teamBlock  `tfsdk:"team"`
 }
 
@@ -28,9 +29,9 @@ type testSuiteV6Model struct {
 	ApiToken      types.String `tfsdk:"api_token"`
 	DefaultBranch types.String `tfsdk:"default_branch"`
 	ID            types.String `tfsdk:"id"`
+	UUID          types.String `tfsdk:"uuid"`
 	Name          types.String `tfsdk:"name"`
 	Slug          types.String `tfsdk:"slug"`
-	WebUrl        types.String `tfsdk:"web_url"`
 	Team          []teamBlock  `tfsdk:"team"`
 	Teams         []teamBlock  `tfsdk:"teams"`
 }
@@ -38,16 +39,21 @@ type testSuiteV6Model struct {
 type testSuiteResponse struct {
 	ApiToken      string `json:"api_token"`
 	DefaultBranch string `json:"default_branch"`
-	ID            string `json:"id"`
+	UUID          string `json:"id"`
+	GraphqlID     string `json:"graphql_id"`
 	Name          string `json:"name"`
 	Slug          string `json:"slug"`
-	WebUrl        string `json:"web_url"`
 	TeamUuids     string `json:"team_ids"`
 }
 
 type teamBlock struct {
-	ID          types.String `tfsdk:"id"`
 	AccessLevel types.String `tfsdk:"access_level"`
+	ID          types.String `tfsdk:"id"`
+	TeamSuiteID types.String `tfsdk:"team_suite_id"`
+}
+
+type teamMap struct {
+	id, uuid string
 }
 
 type testSuiteResource struct {
@@ -75,12 +81,20 @@ func (ts *testSuiteResource) Create(ctx context.Context, req resource.CreateRequ
 	var plan, state testSuiteModel
 	var v6Plan, v6State testSuiteV6Model
 	var response testSuiteResponse
+	var allTeams []teamBlock
+	var manageTeams, readTeams []teamMap
 	payload := map[string]interface{}{}
 
 	if ts.isProtoV6 {
 		resp.Diagnostics.Append(req.Plan.Get(ctx, &v6Plan)...)
+		if len(v6Plan.Teams) > 0 {
+			allTeams = v6Plan.Teams
+		} else if len(v6Plan.Team) > 0 {
+			allTeams = v6Plan.Team
+		}
 	} else {
 		resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+		allTeams = plan.Team
 	}
 	if resp.Diagnostics.HasError() {
 		return
@@ -88,21 +102,29 @@ func (ts *testSuiteResource) Create(ctx context.Context, req resource.CreateRequ
 
 	// The REST API requires team UUIDs but everything else in the provider uses GraphQL IDs. So we map from UUID to ID
 	// here
-	allTeams := append(plan.Team, v6Plan.Teams...)
-	teamUuids := make([]string, len(allTeams))
+	manageTeamUuids := make([]string, len(allTeams))
 	for i, team := range allTeams {
 		apiResponse, err := getNode(ts.client.genqlient, team.ID.ValueString())
 		if err != nil {
+			// TODO
 		}
-		if team, ok := apiResponse.Node.(*getNodeNodeTeam); ok {
-			teamUuids[i] = team.Uuid
+		if apiTeam, ok := apiResponse.Node.(*getNodeNodeTeam); ok {
+			t := teamMap{id: apiTeam.Id, uuid: apiTeam.Uuid}
+			if team.AccessLevel.ValueString() == string(SuiteAccessLevelsManageAndRead) {
+				manageTeams = append(manageTeams, t)
+				manageTeamUuids[i] = apiTeam.Uuid
+			} else if team.AccessLevel.ValueString() == string(SuiteAccessLevelsReadOnly) {
+				readTeams = append(readTeams, t)
+			} else {
+				// TODO: error? unknown permission level
+			}
 		}
 	}
 
 	payload["name"] = plan.Name.ValueString()
 	payload["default_branch"] = plan.DefaultBranch.ValueString()
 	payload["show_api_token"] = true
-	payload["team_ids"] = teamUuids
+	payload["team_ids"] = manageTeamUuids
 
 	url := fmt.Sprintf("/v2/analytics/organizations/%s/suites", ts.client.organization)
 	err := ts.client.makeRequest("POST", url, payload, &response)
@@ -111,39 +133,43 @@ func (ts *testSuiteResource) Create(ctx context.Context, req resource.CreateRequ
 	}
 
 	// REST only allows adding teams to a suite with MANAGE_AND_READ so we have to manage the others manually
-	apiResponse, err := findTeamSuite(ts.client.genqlient, ts.client.organization, plan.Name.ValueString(), len(allTeams))
-	teams := make(map[string]string)
-	for _, suite := range apiResponse.Organization.Suites.Edges {
-		for _, teamSuite := range suite.Node.Teams.Edges {
-			teams[teamSuite.Node.Team.Id] = teamSuite.Node.Id
+	graphqlResponse, err := listTeamSuites(ts.client.genqlient, ts.client.organization, plan.Name.ValueString(), 50)
+	var suiteId string
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to link team to test suite", err.Error())
+	}
+	for _, resp := range graphqlResponse.Organization.Suites.Edges {
+		if resp.Node.Uuid == response.UUID {
+			suiteId = resp.Node.Id
+			break
 		}
 	}
-	for _, team := range allTeams {
-		// if this team is already MANAGE_AND_READ then skip it
-		if team.AccessLevel.ValueString() == string(SuiteAccessLevelsManageAndRead) {
-			continue
-		}
-		_, err := updateTestSuiteTeam(ts.client.genqlient, teams[team.ID.ValueString()], SuiteAccessLevels(team.AccessLevel.ValueString()))
+	if suiteId == "" {
+		resp.Diagnostics.AddError("Could not find the created suite in Graphql", "This may have left a dangling suite. Please check the UI")
+		return
+	}
+	// Now we can add the READ_ONLY teams to the suite
+	for _, readTeam := range readTeams {
+		_, err := createTestSuiteTeam(ts.client.genqlient, readTeam.id, suiteId, SuiteAccessLevelsReadOnly)
 		if err != nil {
 			resp.Diagnostics.AddError("Failed to link team to test suite", err.Error())
-			return
 		}
 	}
 
 	state.ApiToken = types.StringValue(response.ApiToken)
 	state.DefaultBranch = types.StringValue(response.DefaultBranch)
-	state.ID = types.StringValue(response.ID)
+	state.ID = types.StringValue(response.UUID)
+	state.UUID = types.StringValue(suiteId)
 	state.Name = types.StringValue(response.Name)
 	state.Slug = types.StringValue(response.Slug)
-	state.WebUrl = types.StringValue(response.WebUrl)
 	state.Team = plan.Team
 	if ts.isProtoV6 {
 		v6State.ApiToken = types.StringValue(response.ApiToken)
 		v6State.DefaultBranch = types.StringValue(response.DefaultBranch)
-		v6State.ID = types.StringValue(response.ID)
+		v6State.ID = types.StringValue(response.UUID)
+		v6State.UUID = types.StringValue(suiteId)
 		v6State.Name = types.StringValue(response.Name)
 		v6State.Slug = types.StringValue(response.Slug)
-		v6State.WebUrl = types.StringValue(response.WebUrl)
 		v6State.Team = plan.Team
 		v6State.Teams = v6Plan.Teams
 		resp.Diagnostics.Append(resp.State.Set(ctx, v6State)...)
@@ -172,43 +198,72 @@ func (*testSuiteResource) Metadata(ctx context.Context, req resource.MetadataReq
 	resp.TypeName = req.ProviderTypeName + "_test_suite"
 }
 
+func findTeamSuite(uuid string, suites []listTeamSuitesOrganizationSuitesSuiteConnectionEdgesSuiteEdge) *listTeamSuitesOrganizationSuitesSuiteConnectionEdgesSuiteEdgeNodeSuite {
+	for _, suite := range suites {
+		if suite.Node.Uuid == uuid {
+			return &suite.Node
+		}
+	}
+
+	return nil
+}
+
 func (ts *testSuiteResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 	var state testSuiteModel
-	var response testSuiteResponse
+	var v6State testSuiteV6Model
+	var usingTeams bool
 
 	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if ts.isProtoV6 {
+		resp.Diagnostics.Append(req.State.Get(ctx, &v6State)...)
+	}
 	if resp.Diagnostics.HasError() {
 		return
 	}
-
-	url := fmt.Sprintf("/v2/analytics/organizations/%s/suites/%s", ts.client.organization, state.Slug.ValueString())
-	err := ts.client.makeRequest("GET", url, nil, &response)
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to read test suite", err.Error())
+	if len(v6State.Teams) > 0 {
+		usingTeams = true
 	}
 
-	state.DefaultBranch = types.StringValue(response.DefaultBranch)
-	state.WebUrl = types.StringValue(response.WebUrl)
-	state.Name = types.StringValue(response.Name)
+	graphqlResponse, err := getTestSuite(ts.client.genqlient, state.ID.ValueString(), 50)
+	if err != nil {
+		return
+	}
 
-	graphqlResponse, err := findTeamSuite(ts.client.genqlient, ts.client.organization, response.Name, 50)
 	var teams []teamBlock
-	for _, suite := range graphqlResponse.Organization.Suites.Edges {
-		for _, teamSuite := range suite.Node.Teams.Edges {
+	if suite, ok := graphqlResponse.Suite.(*getTestSuiteSuite); ok {
+		for _, teamSuite := range suite.Teams.Edges {
 			teams = append(teams, teamBlock{
-				ID:          types.StringValue(teamSuite.Node.Team.Id),
 				AccessLevel: types.StringValue(string(teamSuite.Node.AccessLevel)),
+				ID:          types.StringValue(teamSuite.Node.Team.Id),
+				TeamSuiteID: types.StringValue(teamSuite.Node.Id),
 			})
 		}
 	}
-	state.Team = teams
 
-	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+	if usingTeams {
+		v6State.Teams = teams
+	} else if ts.isProtoV6 {
+		v6State.Team = teams
+	}
+	if ts.isProtoV6 {
+		resp.Diagnostics.Append(resp.State.Set(ctx, v6State)...)
+	} else {
+		state.Team = teams
+		resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+	}
+
 }
 
 func (ts *testSuiteResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	var deprecationMessage string
 	attributes := map[string]schema.Attribute{
 		"id": schema.StringAttribute{
+			Computed: true,
+			PlanModifiers: []planmodifier.String{
+				stringplanmodifier.UseStateForUnknown(),
+			},
+		},
+		"uuid": schema.StringAttribute{
 			Computed: true,
 			PlanModifiers: []planmodifier.String{
 				stringplanmodifier.UseStateForUnknown(),
@@ -230,19 +285,28 @@ func (ts *testSuiteResource) Schema(ctx context.Context, req resource.SchemaRequ
 		"default_branch": schema.StringAttribute{
 			Required: true,
 		},
-		"web_url": schema.StringAttribute{
-			Computed: true,
-		},
 	}
 
 	if ts.isProtoV6 {
-		attributes["teams"] = schema.ListNestedAttribute{
+		deprecationMessage = ``
+		attributes["teams"] = schema.SetNestedAttribute{
 			Optional: true,
+			Validators: []validator.Set{
+				setvalidator.ExactlyOneOf(path.Expressions{
+					path.MatchRoot("team"),
+					path.MatchRoot("teams"),
+				}...),
+				setvalidator.SizeAtLeast(1),
+				// TODO: need to validate that at least one entry is marked as MANAGE_AND READ
+			},
 			NestedObject: schema.NestedAttributeObject{
 				Attributes: map[string]schema.Attribute{
 					"id": schema.StringAttribute{
 						MarkdownDescription: "The GraphQL ID of the team to give access.",
 						Required:            true,
+					},
+					"team_suite_id": schema.StringAttribute{
+						Computed:            true,
 					},
 					"access_level": schema.StringAttribute{
 						MarkdownDescription: "The access level of the team.",
@@ -259,15 +323,19 @@ func (ts *testSuiteResource) Schema(ctx context.Context, req resource.SchemaRequ
 	resp.Schema = schema.Schema{
 		MarkdownDescription: "A test suite is a collection of tests. A run is to a suite what a build is to a Pipeline.",
 		Blocks: map[string]schema.Block{
-			"team": schema.ListNestedBlock{
-				Validators: []validator.List{
-					listvalidator.SizeAtLeast(1),
+			"team": schema.SetNestedBlock{
+				DeprecationMessage: deprecationMessage,
+				Validators: []validator.Set{
+					setvalidator.SizeAtLeast(1),
 				},
 				NestedObject: schema.NestedBlockObject{
 					Attributes: map[string]schema.Attribute{
 						"id": schema.StringAttribute{
 							MarkdownDescription: "The GraphQL ID of the team to give access.",
 							Required:            true,
+						},
+						"team_suite_id": schema.StringAttribute{
+							Computed:            true,
 						},
 						"access_level": schema.StringAttribute{
 							MarkdownDescription: "The access level of the team.",
