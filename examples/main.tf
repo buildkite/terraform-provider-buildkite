9df537e7707ef09a8eb927df73969d52de6da739
Jarryd Tilbrook <jarryd@buildkite.com>
2023-07-27 13:47:03 +0800

    Add buildkite_test_suite to examples/main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -36,7 +36,7 @@ resource "buildkite_pipeline_schedule" "weekly" {
   label       = "Weekly build from default branch"
   cronline    = "@midnight"
   branch      = "default"
-  message = "Weekly scheduled build"   
+  message = "Weekly scheduled build"
 }
 
 data "buildkite_pipeline" "data2" {
@@ -54,8 +54,14 @@ resource "buildkite_cluster_queue" "queue1" {
 }
 
 resource "buildkite_cluster_agent_token" "token1" {
-  cluster_id = "Q2x1c3Rlci0tLTMzMDc0ZDhiLTM4MjctNDRkNC05YTQ3LTkwN2E2NWZjODViNg==" 
-  description = "agent token for Dev cluster" 
+  cluster_id = "Q2x1c3Rlci0tLTMzMDc0ZDhiLTM4MjctNDRkNC05YTQ3LTkwN2E2NWZjODViNg=="
+  description = "agent token for Dev cluster"
+}
+
+resource "buildkite_test_suite" "unit_tests" {
+  name = "Unit tests"
+  default_branch = "main"
+  team_owner_id = buildkite_team.test.id
 }
 
 output "agent_token" {
