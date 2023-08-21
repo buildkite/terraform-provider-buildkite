251774a01c032b538516e2e295256d3d7bd98a8d
Jarryd Tilbrook <jarryd@buildkite.com>
2023-08-21 13:26:05 +0800

    Change all imports to testing framework

--- buildkite/resource_cluster_agent_token_test.go
+++ buildkite/resource_cluster_agent_token_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func testAccClusterAgentTokenBasic(description string) string {
