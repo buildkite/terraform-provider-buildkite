6f0ae2a1b9d34134c838f8de9c0c45421db33ced
Jarryd Tilbrook <jarryd@buildkite.com>
2023-08-21 22:08:20 +0800

    Update cluster datasource tests

--- buildkite/provider_test.go
+++ buildkite/provider_test.go
@@ -8,6 +8,7 @@ import (
 
 	genqlient "github.com/Khan/genqlient/graphql"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
@@ -33,6 +34,12 @@ func init() {
 	graphqlClient = graphql.NewClient(defaultGraphqlEndpoint, httpClient)
 	genqlientGraphql = genqlient.NewClient(defaultGraphqlEndpoint, httpClient)
 	organizationID, _ = GetOrganizationID(getenv("BUILDKITE_ORGANIZATION_SLUG"), graphqlClient)
+}
+
+func protoV5ProviderFactories() map[string]func() (tfprotov5.ProviderServer, error) {
+	return map[string]func() (tfprotov5.ProviderServer, error){
+		"buildkite": providerserver.NewProtocol5WithError(New("testing")),
+	}
 }
 
 func protoV6ProviderFactories() map[string]func() (tfprotov6.ProviderServer, error) {
