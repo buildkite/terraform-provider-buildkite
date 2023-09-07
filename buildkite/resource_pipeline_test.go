dbd292ddf6166f40eedfe81b53aa36e1628e575b
James Sammut <70425486+james2791@users.noreply.github.com>
2023-09-07 12:18:01 +1000

    Added `build_pull_request_ready_for_review` to pipeline resource docs/examples/tests (#396)
    * Added PR ready for review `provider_settings` attribute
    
    * Example provider_settings config, pipeline create/update all attributes tests amendment
    
    * Changelog
    
    * Sneaky extra space

--- buildkite/resource_pipeline_test.go
+++ buildkite/resource_pipeline_test.go
@@ -181,10 +181,11 @@ func TestAccBuildkitePipeline(t *testing.T) {
 				tags = ["llama"]
 				provider_settings {
 					trigger_mode = "code"
-					build_pull_requests = false
+					build_pull_requests = true
 					skip_builds_for_existing_commits = true
 					build_branches = true
 					build_tags = true
+					build_pull_request_ready_for_review = true
 					cancel_deleted_branch_builds = true
 					filter_enabled = true
 					filter_condition = "true"
@@ -222,10 +223,11 @@ func TestAccBuildkitePipeline(t *testing.T) {
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "skip_intermediate_builds", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "skip_intermediate_builds_branch_filter", "!main"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.trigger_mode", "code"),
-						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.build_pull_requests", "false"),
+						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.build_pull_requests", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.skip_builds_for_existing_commits", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.build_branches", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.build_tags", "true"),
+						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.build_pull_request_ready_for_review", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.cancel_deleted_branch_builds", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.filter_enabled", "true"),
 						resource.TestCheckResourceAttr("buildkite_pipeline.pipeline", "provider_settings.0.filter_condition", "true"),
@@ -292,10 +294,11 @@ func TestAccBuildkitePipeline(t *testing.T) {
 							tags = ["llama"]
 							provider_settings {
 								trigger_mode = "code"
-								build_pull_requests = false
+								build_pull_requests = true
 								skip_builds_for_existing_commits = true
 								build_branches = true
 								build_tags = true
+								build_pull_request_ready_for_review = true
 								cancel_deleted_branch_builds = true
 								filter_enabled = true
 								filter_condition = "true"
