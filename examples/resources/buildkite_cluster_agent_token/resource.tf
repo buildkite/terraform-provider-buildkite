0ae99f34028f00a13789510ae477461fd1359805
Jarryd Tilbrook <jarryd@buildkite.com>
2023-09-11 12:25:13 +0800

    Update docs for cluster agent token resource

---
+++ examples/resources/buildkite_cluster_agent_token/resource.tf
@@ -0,0 +1,22 @@
+resource "buildkite_cluster" "primary" {
+  name        = "Primary cluster"
+  description = "Runs the monolith build and deploy"
+  emoji       = "🚀"
+  color       = "#bada55"
+}
+
+resource "buildkite_cluster_agent_token" "default" {
+  description = "Default cluster token"
+  cluster_id  = buildkite_cluster.primary.id
+}
+
+resource "buildkite_pipeline" "monolith" {
+  name       = "Monolith"
+  repository = "https://github.com/..."
+  cluster_id = buildkite_cluster.primary.id
+}
+
+resource "buildkite_cluster_queue" "default" {
+  cluster_id = buildkite_cluster.primary.id
+  key        = "default"
+}
