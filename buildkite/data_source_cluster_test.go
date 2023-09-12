07b91a4e50a54df2594631cde6b8fef548669bd6
Jarryd Tilbrook <jarryd@buildkite.com>
2023-09-12 20:11:44 +0800

    SUP-1370 Update to protocol v6 (#400)
    * Update to protocol v6
    
    * Update changelog

--- buildkite/data_source_cluster_test.go
+++ buildkite/data_source_cluster_test.go
@@ -14,7 +14,7 @@ func TestAccBuildkiteClusterDatasource(t *testing.T) {
 		clusterName := acctest.RandString(12)
 
 		resource.ParallelTest(t, resource.TestCase{
-			ProtoV5ProviderFactories: protoV5ProviderFactories(),
+			ProtoV6ProviderFactories: protoV6ProviderFactories(),
 			Steps: []resource.TestStep{
 				{
 					Config: fmt.Sprintf(`
@@ -38,7 +38,7 @@ func TestAccBuildkiteClusterDatasource(t *testing.T) {
 	t.Run("can find a cluster", func(t *testing.T) {
 		clusterName := acctest.RandString(12)
 		resource.ParallelTest(t, resource.TestCase{
-			ProtoV5ProviderFactories: protoV5ProviderFactories(),
+			ProtoV6ProviderFactories: protoV6ProviderFactories(),
 			Steps: []resource.TestStep{
 				{
 					Config: fmt.Sprintf(`
@@ -60,7 +60,7 @@ func TestAccBuildkiteClusterDatasource(t *testing.T) {
 
 	t.Run("errors if cannot find cluster", func(t *testing.T) {
 		resource.ParallelTest(t, resource.TestCase{
-			ProtoV5ProviderFactories: protoV5ProviderFactories(),
+			ProtoV6ProviderFactories: protoV6ProviderFactories(),
 			Steps: []resource.TestStep{
 				{
 					Config: `data "buildkite_cluster" "default" {
