146a8013925af992b874c81d11eee34acd210271
Jarryd Tilbrook <jarryd@buildkite.com>
2023-09-21 14:28:08 +0800

    Generate docs

--- examples/resources/buildkite_organization/resource.tf
+++ examples/resources/buildkite_organization/resource.tf
@@ -1,4 +1,5 @@
-# allow api access only from 1.1.1.1
+# allow api access only from 1.1.1.1 and enforce 2fa for all members
 resource "buildkite_organization" "settings" {
   allowed_api_ip_addresses = ["1.1.1.1/32"]
+  enforce_2fa              = true
 }
