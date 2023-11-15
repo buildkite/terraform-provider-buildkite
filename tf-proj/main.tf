ae15041f1b8100084e878abcfaf2ea0e20490835
Narthana Epa <narthana.epa@gmail.com>
2023-11-15 15:08:38 +1100

    Back to old tf example

--- tf-proj/main.tf
+++ tf-proj/main.tf
@@ -7,25 +7,12 @@ terraform {
   }
 }
 
-provider "buildkite" {}
-variable "thing_to_echo" {
-  type    = string
-  default = "hello world"
+provider "buildkite" {
+  organization = "testkite"
 }
 
-resource "buildkite_pipeline" "test-signing-pipeline" {
-  name       = "test-signing-pipeline"
-  repository = "https://github.com/moskyb/bash-example"
 
-  signed_steps_input = <<EOF
-  steps:
-    - label: ":bash:"
-      command: "echo '${var.thing_to_echo}'"
-    - label: gidday
-      command: "echo 'this is another cool step!'"
-    - command: "echo 'and one more'"
-  EOF
-
-  signing_jwks   = file("/Users/ben/signing.json")
-  signing_key_id = "harold"
+resource "buildkite_cluster" "test_cluster" {
+  name = "cluster-test"
+  description = "test cluster"
 }
