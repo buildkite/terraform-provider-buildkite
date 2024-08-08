6a8469703187050297e226d492833090ce89c53a
James Sammut <juma2791@gmail.com>
2024-08-08 16:31:21 +1000

    First organization rule resource/test implementation, GQL schema

---
+++ buildkite/resource_organization_rule.go
@@ -0,0 +1,144 @@
+package buildkite
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+)
+
+type organizationRuleResourceModel struct {
+	Id          types.String `tfsdk:"id"`
+	Uuid        types.String `tfsdk:"uuid"`
+    Name        types.String `tfsdk:"name"` 
+	Value       types.String `tfsdk:"value"` 
+	SourceType  types.String `tfsdk:"source_type"`
+	SourceUuid  types.String `tfsdk:"source_uuid"` 
+	TargetType  types.String `tfsdk:"target_type"`
+	TargetUuid  types.String `tfsdk:"target_uuid"`
+	Effect      types.String `tfsdk:"effect"`
+	Action      types.String `tfsdk:"action"`
+}
+
+type organizationRuleResource struct {
+	client *Client
+}
+
+func newOrganizationRuleResource() resource.Resource {
+	return &organizationRuleResource{}
+}
+
+func (organizationRuleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_organization_rule"
+}
+
+func (or *organizationRuleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	or.client = req.ProviderData.(*Client)
+}
+
+func (organizationRuleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = resource_schema.Schema{
+		MarkdownDescription: "An Organization Rule allows specifying explicit rules between two Buildkite resources and desired effect and actions. ",
+		Attributes: map[string]resource_schema.Attribute{
+			"id": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The GraphQL ID of the organization rule.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"uuid": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The UUID of the organization rule.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": resource_schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The name that is given to this organization rule.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"value": resource_schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The JSON rule that this organization rule implements.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"source_type": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The source resource type that this organization rule allows or denies to invoke its defined action. ",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"source_uuid": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The UUID of the resource that this organization rule allows or denies invocating its defined action. ",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"target_type": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The target resource type that this organization rule allows or denies the source to respective action. ",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"target_uuid": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The UUID of the target resourcee that this organization rule allows or denies invocation its respective action. ",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"effect": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Whether this organization rule allows or denys the action to take place between source and target resources. ",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"action": resource_schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The action defined between source and target resources. ",
+			},
+		},
+	}
+}
+
+func (cq *organizationRuleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+}
+
+func (cq *organizationRuleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+}
+
+func (cq *organizationRuleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+
+}
+
+func (cq *organizationRuleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+
+}
+
+func (cq *organizationRuleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	
+}
