d21db63a96494c96d04ede9cac6685835adc8cbe
James Sammut <juma2791@gmail.com>
2024-09-02 12:09:29 +1000

    Docs alignment for the source|target_pipeline_uuid keys in a rule document

--- examples/resources/buildkite_organization_rule/resource.tf
+++ examples/resources/buildkite_organization_rule/resource.tf
@@ -1,15 +1,15 @@
 resource "buildkite_organization_rule" "trigger_build_test_dev" {
     type = "pipeline.trigger_build.pipeline"
     value = jsonencode({
-        triggering_pipeline_uuid = buildkite_pipeline.app_dev_deploy.uuid
-        triggered_pipeline_uuid = buildkite_pipeline.app_test_ci.uuid
+        source_pipeline_uuid = buildkite_pipeline.app_dev_deploy.uuid
+        target_pipeline_uuid = buildkite_pipeline.app_test_ci.uuid
     })
 }
 
 resource "buildkite_organization_rule" "artifacts_read_test_dev" {
     type = "pipeline.artifacts_read.pipeline"
     value = jsonencode({
-        target_pipeline_uuid = buildkite_pipeline.app_dev_deploy.uuid
         source_pipeline_uuid = buildkite_pipeline.app_test_ci.uuid
+        target_pipeline_uuid = buildkite_pipeline.app_dev_deploy.uuid
     })
 }
