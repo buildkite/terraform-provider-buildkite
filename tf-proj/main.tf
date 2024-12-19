42ae2ff60992383577894e073e2ecfc3aa0264cd
CerealBoy <allan@buildkite.com>
2024-12-19 17:25:26 +1100

    Bring the project back closer to the original

--- tf-proj/main.tf
+++ tf-proj/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "buildkite" {
-  organization = "al-dev-env"
+  organization = "testkite"
 }
 
 resource "buildkite_cluster" "test_cluster" {
@@ -16,36 +16,36 @@ resource "buildkite_cluster" "test_cluster" {
   description = "test cluster"
 }
 
-#resource "buildkite_cluster_queue" "hosted_linux_small" {
-#  cluster_id  = buildkite_cluster.test_cluster.id
-#  key         = "hosted-linux-small"
-#  description = "Terraform'd queue for Hosted Linux"
-#
-#  hosted_agents = {
-#    instance_shape = "LINUX_ARM64_2X4"
-#
-#    linux = {
-#      image_agent_ref = "elixir:1.17.3-slim"
-#    }
-#  }
-#}
+resource "buildkite_cluster_queue" "self_hosted" {
+  cluster_id  = buildkite_cluster.test_cluster.id
+  key         = "self-hosted-queue"
+  description = "A self-hosted Terraform-managed queue"
+}
 
-#resource "buildkite_cluster_queue" "self_hosted" {
-#  cluster_id  = buildkite_cluster.test_cluster.id
-#  key         = "self-hosted-queue"
-#  description = "A self-hosted Terraform'd queue"
-#}
+resource "buildkite_cluster_queue" "hosted_linux_small" {
+  cluster_id  = buildkite_cluster.test_cluster.id
+  key         = "hosted-linux-small"
+  description = "Terraform queue for Hosted Linux"
 
-#resource "buildkite_cluster_queue" "hosted_macos_small" {
-#  cluster_id  = buildkite_cluster.test_cluster.id
-#  key         = "hosted-macos-small"
-#  description = "MacOS hosted agents via Terraform"
-#
-#  hosted_agents = {
-#    instance_shape = "MACOS_M2_4X7"
-#
-#    mac = {
-#      xcode_version = "16.2"
-#    }
-#  }
-#}
+  hosted_agents = {
+    instance_shape = "LINUX_ARM64_2X4"
+
+    linux = {
+      image_agent_ref = "elixir:1.17.3-slim"
+    }
+  }
+}
+
+resource "buildkite_cluster_queue" "hosted_macos_small" {
+  cluster_id  = buildkite_cluster.test_cluster.id
+  key         = "hosted-macos-small"
+  description = "MacOS hosted agents via Terraform"
+
+  hosted_agents = {
+    instance_shape = "MACOS_M2_4X7"
+
+    mac = {
+      xcode_version = "16.2"
+    }
+  }
+}
