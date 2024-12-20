8bc74243b34cd159a63cf06624ec6557c2d2525d
Tom Watt <tom@buildkite.com>
2024-12-20 12:32:34 +0000

    fix: add PlanModifiers when Hosted Agent attributes changed * Queues cannot change from Self-Hosted to Hosted (and vice-versa) so Resource should be recreated * instance_shape cannot be updated in-place so Resource should be recreated to change it
    Signed-off-by: Tom Watt <tom@buildkite.com>

--- buildkite/resource_cluster_queue.go
+++ buildkite/resource_cluster_queue.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -137,6 +138,9 @@ func (clusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 				Validators: []validator.Object{
 					&hostedAgentValidator{},
 				},
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.RequiresReplace(),
+				},
 				Attributes: map[string]resource_schema.Attribute{
 					"mac": resource_schema.SingleNestedAttribute{
 						Optional: true,
@@ -175,6 +179,9 @@ func (clusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 								LinuxARM64InstanceLarge,
 								LinuxARM64InstanceXLarge,
 							),
+						},
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.RequiresReplace(),
 						},
 					},
 				},
