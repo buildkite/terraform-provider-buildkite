cbd14b8b9ec4dd6c4f4854bede9d26a67c46d782
Tom Watt <tom@buildkite.com>
2024-12-20 19:38:18 +0000

    SUP-2591: add PlanModifiers when Hosted Agent attributes changed (#598)

--- buildkite/resource_cluster_queue.go
+++ buildkite/resource_cluster_queue.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -137,6 +138,29 @@ func (clusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 				Validators: []validator.Object{
 					&hostedAgentValidator{},
 				},
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.RequiresReplaceIf(func(ctx context.Context, or planmodifier.ObjectRequest, rrifr *objectplanmodifier.RequiresReplaceIfFuncResponse) {
+						var data *struct {
+							Mac           types.Object `tfsdk:"mac"`
+							Linux         types.Object `tfsdk:"linux"`
+							InstanceShape types.String `tfsdk:"instance_shape"`
+						}
+
+						rrifr.Diagnostics.Append(or.ConfigValue.As(ctx, &data, basetypes.ObjectAsOptions{})...)
+
+						if rrifr.Diagnostics.HasError() {
+							return
+						}
+
+						// If the hosted_agents attribute is added or removed e.g., change from a Hosted Agent to Self-Hosted Agent Queue
+						if or.StateValue.IsNull() && !or.PlanValue.IsNull() || or.ConfigValue.IsNull() {
+							rrifr.RequiresReplace = true
+							return
+						}
+
+						rrifr.RequiresReplace = false
+					}, "Recreates the resource if the hosted_agents attribute is added or removed.", "Recreates the resource if the hosted_agents attribute is added or removed."),
+				},
 				Attributes: map[string]resource_schema.Attribute{
 					"mac": resource_schema.SingleNestedAttribute{
 						Optional: true,
@@ -175,6 +199,9 @@ func (clusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 								LinuxARM64InstanceLarge,
 								LinuxARM64InstanceXLarge,
 							),
+						},
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.RequiresReplace(),
 						},
 					},
 				},
