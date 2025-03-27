31ee6c226d66f272d455126546af2567772c6902
Micheal <79476489+Damilola-obasa@users.noreply.github.com>
2025-03-27 11:36:17 -0400

    update buildkite_pipeline docs (#895)
    Co-authored-by: micheal <damilolaobasa1@gmail.com>

--- examples/resources/buildkite_pipeline/resource.tf
+++ examples/resources/buildkite_pipeline/resource.tf
@@ -1,15 +1,23 @@
 # minimal repository
+data "buildkite_cluster" "default" {
+  name = "Default cluster"
+}
 resource "buildkite_pipeline" "pipeline" {
   name       = "repo"
   repository = "git@github.com:my-org/my-repo"
+  cluster_id = data.buildkite_cluster.default.id
 }
 
 # with github provider settings
+data "buildkite_cluster" "default" {
+  name = "Default cluster"
+}
 resource "buildkite_pipeline" "pipeline" {
   color      = "#000000"
   emoji      = ":buildkite:"
   name       = "repo"
   repository = "git@github.com:my-org/my-repo"
+  cluster_id = data.buildkite_cluster.default.id
 
   provider_settings = {
     build_branches      = false
@@ -20,6 +28,9 @@ resource "buildkite_pipeline" "pipeline" {
 }
 
 # signed pipeline
+data "buildkite_cluster" "default" {
+  name = "Default cluster"
+}
 locals {
   repository = "git@github.com:my-org/my-repo.git"
 }
@@ -39,5 +50,60 @@ YAML
 resource "buildkite_pipeline" "signed-pipeline" {
   name       = "my-signed-pipeline"
   repository = local.repository
+  cluster_id = data.buildkite_cluster.default.id
   steps      = data.buildkite_signed_pipeline_steps.signed-steps.steps
 }
+
+
+# Advanced example using Github provider to create repository webhook for Buildkite pipeline
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    buildkite = {
+      source  = "buildkite/buildkite"
+      version = "~> 1.16"
+    }
+  }
+}
+
+provider "github" {}
+provider "buildkite" {
+  organization = "my-org-slug"
+}
+
+resource "github_repository" "test_repo" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "private"
+}
+
+data "buildkite_cluster" "default" {
+  name = "Default cluster"
+}
+
+resource "buildkite_pipeline" "pipeline" {
+  name       = "repo"
+  repository = github_repository.test_repo.ssh_clone_url
+  cluster_id = data.buildkite_cluster.default.id
+  depends_on = [github_repository.test_repo]
+}
+
+resource "github_repository_webhook" "my_webhook" {
+  repository = github_repository.test_repo.name
+
+  configuration {
+    url          = buildkite_pipeline.pipeline.webhook_url
+    content_type = "application/json"
+    insecure_ssl = false
+  }
+
+  events = ["deployment", "pull_request", "push"]
+
+  depends_on = [buildkite_pipeline.pipeline]
+}
+
