3d8a404d304a909b26e16c03172e4e46a9e69250
Pete Tomasik <senate6268@gmail.com>
2025-04-01 18:44:30 -0400

    Restore commented out tests

--- buildkite/resource_cluster_queue_test.go
+++ buildkite/resource_cluster_queue_test.go
@@ -83,11 +83,9 @@ func TestAccBuildkiteClusterQueueResource(t *testing.T) {
         description = "Acceptance test %s"
 
         hosted_agents = {
-            # re-enable when macos hosted cluster queue creation is fixed
-            # https://github.com/buildkite/terraform-provider-buildkite/issues/869
-            # mac = {
-            #     xcode_version = "14.3.1"
-            # }
+            mac = {
+                xcode_version = "14.3.1"
+            }
             instance_shape = "MACOS_M2_4X7"
         }
     }
@@ -391,7 +389,7 @@ func TestAccBuildkiteClusterQueueResource(t *testing.T) {
 			resource.TestCheckResourceAttr("buildkite_cluster_queue.foobar", "hosted_agents.instance_shape", "MACOS_M2_4X7"),
 			// re-enable when macos hosted cluster queue creation is fixed
 			// https://github.com/buildkite/terraform-provider-buildkite/issues/869
-			// resource.TestCheckResourceAttr("buildkite_cluster_queue.foobar", "hosted_agents.mac.xcode_version", "14.3.1"),
+			resource.TestCheckResourceAttr("buildkite_cluster_queue.foobar", "hosted_agents.mac.xcode_version", "14.3.1"),
 		)
 
 		resource.ParallelTest(t, resource.TestCase{
