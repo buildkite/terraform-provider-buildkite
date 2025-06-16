ab761c630a0f0242f5ce32d63e8ef6daac729437
Tom Watt <tom@buildkite.com>
2025-06-16 17:33:41 +0100

    feat: update logic for importing Test Suite API Token into State to handle if the Token is regenerated

--- buildkite/resource_test_suite.go
+++ buildkite/resource_test_suite.go
@@ -235,23 +235,29 @@ func (ts *testSuiteResource) Read(ctx context.Context, req resource.ReadRequest,
 		resp.State.RemoveResource(ctx)
 	}
 
-	// If Test Suite Imported, API Token will be null, so need to fetch it to set it in State
-	if state.ApiToken.IsNull() {
-		var response testSuiteResponse
+	// API Token only available from REST API
+	var response testSuiteResponse
 
-		// Construct URL to call to the REST API
-		url := fmt.Sprintf("/v2/analytics/organizations/%s/suites/%s?show_api_token=true", ts.client.organization, state.Slug.ValueString())
-		err = retry.RetryContext(ctx, timeout, func() *retry.RetryError {
-			err := ts.client.makeRequest(ctx, "GET", url, nil, &response)
+	// Construct URL to call to the REST API to get the API Token
+	url := fmt.Sprintf("/v2/analytics/organizations/%s/suites/%s?show_api_token=true", ts.client.organization, state.Slug.ValueString())
+	err = retry.RetryContext(ctx, timeout, func() *retry.RetryError {
+		err := ts.client.makeRequest(ctx, "GET", url, nil, &response)
+		return retryContextError(err)
+	})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to read test suite API token",
+			fmt.Sprintf("Failed to read test suite API token: %s", err.Error()),
+		)
+		return
+	}
 
-			return retryContextError(err)
-		})
-		if err != nil {
-			resp.Diagnostics.AddError(
-				"Failed to read test suite API token",
-				fmt.Sprintf("Failed to read test suite API token: %s", err.Error()),
-			)
-			return
+	// Update API Token in State if it has changed or if it is null from importing into State
+	if response.ApiToken != state.ApiToken.ValueString() || state.ApiToken.IsNull() {
+		// The API Token can be regenerated, but 'terraform refresh' or 'terraform apply' is required to update State
+		// don't need a warning if it is null from importing into State
+		if !state.ApiToken.IsNull() {
+			resp.Diagnostics.AddAttributeWarning(path.Root("api_token"), "Test Suite API Token has changed", "Test Suite API Token has changed, \"run terraform refresh\" or \"terraform apply\" to update State")
 		}
 		state.ApiToken = types.StringValue(response.ApiToken)
 	}
