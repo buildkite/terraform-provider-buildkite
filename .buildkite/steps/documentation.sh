fb1d401f8f60a1278d724328372bafbd80c6fceb
Tom Watt <tom@buildkite.com>
2025-07-02 13:29:38 +0100

    chore: remove deprecated dependabot reviewers and annotation fixes (#947)
    * chore: Remove depcreated Dependabot Reviewers as CODOWNERS configured
    
    * fix: remove backticks causing execution of make command obscuring log output
    
    * fix: add context to annotation so it is not overwritten by test annotations
    
    * chore: add context to annotation to avoid future overwritting of default context
    
    * docs/fix: update index.md with 'make docs'

--- .buildkite/steps/documentation.sh
+++ .buildkite/steps/documentation.sh
@@ -12,10 +12,10 @@ git diff --exit-code docs &>/dev/null && true
 docs_changes="$?"
 
 if [ "${docs_changes:-0}" -ne 0 ] ; then
-	echo "+++ :bk-status-failed: Documentation changes detected!!! Run `make docs` locally, commit the changes and re-push."
+	echo "+++ :bk-status-failed: Documentation changes detected!!! Run 'make docs' locally, commit the changes and re-push."
 	git checkout go.* &>/dev/null
 	git status --short | tee git_diff_output.out
-	printf '```term\ngit status --short\n%b\n```' "$(cat git_diff_output.out)" | buildkite-agent annotate --style warning
+	printf '```term\ngit status --short\n%b\n```' "$(cat git_diff_output.out)" | buildkite-agent annotate --style warning --context "docs"
 fi
 
 exit "$docs_changes"
